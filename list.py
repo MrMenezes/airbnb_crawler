1e6631b15e9210ad85febe4c7f0d9e6f92daae14
Mr Menezes <erick.menezes@cubos.io>
2020-12-05 23:37:15 -0300

    feat(children_infant): added check price off children and infants

--- list.py
+++ list.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import sys
 
 
-def get_total(id_value, check_in, check_out, adults):
+def get_total(id_value, check_in, check_out, adults, children=0, infants=0):
     headers = {
         'x-airbnb-api-key': 'd306zoyjsyarp7ifhu67rjxn52tv0t20',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
@@ -16,7 +16,7 @@ def get_total(id_value, check_in, check_out, adults):
         ('locale', 'pt'),
         ('currency', 'BRL'),
         ('variables', '{"request":{"id":"'+str(id_value) +
-         '","layouts":[],"adults":"'+str(adults)+'","sectionIds":["BOOK_IT_SIDEBAR"],"checkIn":"'+check_in+'","checkOut":"'+check_out+'"}}'),
+         '","layouts":[],"adults":"'+str(adults)+'","children":"'+str(children)+'","infants":"'+str(infants)+'","sectionIds":["BOOK_IT_SIDEBAR"],"checkIn":"'+check_in+'","checkOut":"'+check_out+'"}}'),
         ('extensions',
          '{"persistedQuery":{"version":1,"sha256Hash":"c4e4690265fdbd38d7819a8eb702dde2d92398761ae85881c62bd04e0585491b"}}'),
     )
@@ -62,8 +62,8 @@ def get_wishlists(wishlists, check_in, check_out, adults, total=None):
         simple_data['pictureUrl'] = item['listing']['pictureUrl']
         simple_data['publicAddress'] = item['listing']['publicAddress']
         simple_data['spaceType'] = item['listing']['spaceType']
-        simple_data['url'] = 'https://www.airbnb.com.br/rooms/{id}?adults={adults}&check_in={check_in}&check_out={check_out}'.format(
-            id=item_id[0], adults=adults, check_in=check_in, check_out=check_out)
+        simple_data['url'] = 'https://www.airbnb.com.br/rooms/{id}?adults={adults}'.format(
+            id=item_id[0], adults=adults)
         if total is not None:
             total_item = get_total(
                 item_id[0], check_in, check_out, adults)
